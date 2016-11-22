2cb28f6f7943cc4d98cf86ccbf1b2ce6a4dc7452
Dominik <dominik.beutel@student.kit.edu>
2016-11-22 15:48:35 +0100

    Herwigpp Dummy fragment added

---
+++ Herwigpp_DummyProcessNeu_cff_py_GEN.py
@@ -0,0 +1,165 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Herwigpp_DummyProcessNeu_cff.py --fileout file:GEN.root -s GEN --conditions auto:mc --datatier GEN-SIM-RAW --customise Configuration/DataProcessing/Utils.addMonitoring --no_exec -n 5
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedNominalCollision2015_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(5)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.19 $'),
+    annotation = cms.untracked.string('Herwigpp_DummyProcessNeu_cff.py nevts:5'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('file:GEN.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-RAW')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')
+
+process.generator = cms.EDFilter("Herwig7GeneratorFilter",
+    hwpp_pdf_CTEQ6L1 = cms.vstring('+hwpp_pdf_CTEQ6L1_Common', 
+        '+hwpp_ue_EE5C'),
+    hwpp_pdf_CTEQ6LL_Hard = cms.vstring('+hwpp_pdf_CTEQ6L1_Hard'),
+    hwpp_setParticlesStableForDetector = cms.vstring('set /Herwig/Particles/mu-:Stable Stable', 
+        'set /Herwig/Particles/mu+:Stable Stable', 
+        'set /Herwig/Particles/Sigma-:Stable Stable', 
+        'set /Herwig/Particles/Sigmabar+:Stable Stable', 
+        'set /Herwig/Particles/Lambda0:Stable Stable', 
+        'set /Herwig/Particles/Lambdabar0:Stable Stable', 
+        'set /Herwig/Particles/Sigma+:Stable Stable', 
+        'set /Herwig/Particles/Sigmabar-:Stable Stable', 
+        'set /Herwig/Particles/Xi-:Stable Stable', 
+        'set /Herwig/Particles/Xibar+:Stable Stable', 
+        'set /Herwig/Particles/Xi0:Stable Stable', 
+        'set /Herwig/Particles/Xibar0:Stable Stable', 
+        'set /Herwig/Particles/Omega-:Stable Stable', 
+        'set /Herwig/Particles/Omegabar+:Stable Stable', 
+        'set /Herwig/Particles/pi+:Stable Stable', 
+        'set /Herwig/Particles/pi-:Stable Stable', 
+        'set /Herwig/Particles/K+:Stable Stable', 
+        'set /Herwig/Particles/K-:Stable Stable', 
+        'set /Herwig/Particles/K_S0:Stable Stable', 
+        'set /Herwig/Particles/K_L0:Stable Stable'),
+    hwpp_ue_EE5CEnergyExtrapol = cms.vstring('set /Herwig/UnderlyingEvent/MPIHandler:EnergyExtrapolation Power', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:ReferenceScale 7000.*GeV', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:Power 0.33', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:pTmin0 3.91*GeV'),
+    hwpp_pdf_CTEQ6L1_Hard_CUETHS1 = cms.vstring('+hwpp_pdf_CTEQ6L1_Hard_Common', 
+        '+hwpp_ue_CUETHS1'),
+    hwpp_pdf_CTEQ6LL_CUETHS1 = cms.vstring('+hwpp_pdf_CTEQ6L1_CUETHS1'),
+    hwpp_pdf_CTEQ6L1_Hard = cms.vstring('+hwpp_pdf_CTEQ6L1_Hard_Common', 
+        '+hwpp_ue_EE5C'),
+    generatorModule = cms.string('/Herwig/Generators/LHCGenerator'),
+    hwpp_ue_EE5C = cms.vstring('+hwpp_ue_EE5CEnergyExtrapol', 
+        'set /Herwig/Hadronization/ColourReconnector:ColourReconnection Yes', 
+        'set /Herwig/Hadronization/ColourReconnector:ReconnectionProbability 0.49', 
+        'set /Herwig/Partons/RemnantDecayer:colourDisrupt 0.80', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:InvRadius 2.30', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:softInt Yes', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:twoComp Yes', 
+        'set /Herwig/UnderlyingEvent/MPIHandler:DLmode 2'),
+    hwpp_cmsDefaults = cms.vstring('+hwpp_basicSetup', 
+        '+hwpp_setParticlesStableForDetector'),
+    hwpp_pdf_CTEQ6L1_Hard_Common = cms.vstring('create ThePEG::LHAPDF /Herwig/Partons/cmsHardPDFSet ThePEGLHAPDF.so', 
+        'set /Herwig/Partons/cmsHardPDFSet:PDFName cteq6ll.LHpdf', 
+        'set /Herwig/Partons/cmsHardPDFSet:RemnantHandler /Herwig/Partons/HadronRemnants'),
+    run = cms.string('InterfaceTest'),
+    repository = cms.string('HerwigDefaults.rpo'),
+    dataLocation = cms.string('${HERWIGPATH}'),
+    hwpp_pdf_CTEQ6LL_Hard_CUETHS1 = cms.vstring('+hwpp_pdf_CTEQ6L1_Hard_CUETHS1'),
+    filterEfficiency = cms.untracked.double(1.0),
+    hwpp_pdf_CTEQ6L1_CUETHS1 = cms.vstring('+hwpp_pdf_CTEQ6L1_Common', 
+        '+hwpp_ue_CUETHS1'),
+    configFiles = cms.vstring(),
+    parameterSets = cms.vstring('hwpp_cmsDefaults', 
+        'hwpp_ue_EE5C', 
+        'hwpp_pdf_CTEQ6L1', 
+        'hwpp_cm_13TeV', 
+        'dummyprocess'),
+    hwpp_pdf_CTEQ6L1_Common = cms.vstring('create ThePEG::LHAPDF /Herwig/Partons/cmsPDFSet ThePEGLHAPDF.so', 
+        'set /Herwig/Partons/cmsPDFSet:PDFName cteq6ll.LHpdf', 
+        'set /Herwig/Partons/cmsPDFSet:RemnantHandler /Herwig/Partons/HadronRemnants', 
+        'set /Herwig/Particles/p+:PDF /Herwig/Partons/cmsPDFSet', 
+        'set /Herwig/Particles/pbar-:PDF /Herwig/Partons/cmsPDFSet'),
+    dummyprocess = cms.vstring('insert /Herwig/MatrixElements/SimpleQCD:MatrixElements[0] /Herwig/MatrixElements/MEPP2ttbarH'),
+    crossSection = cms.untracked.double(-1),
+    eventHandlers = cms.string('/Herwig/EventHandlers'),
+    hwpp_cm_13TeV = cms.vstring('set /Herwig/Generators/LHCGenerator:EventHandler:LuminosityFunction:Energy 13000.0', 
+        'set /Herwig/Shower/Evolver:IntrinsicPtGaussian 2.2*GeV'),
+    hwpp_basicSetup = cms.vstring('#create ThePEG::RandomEngineGlue /Herwig/RandomGlue', 
+        '#set /Herwig/Generators/LHCGenerator:RandomNumberGenerator /Herwig/RandomGlue', 
+        'set /Herwig/Generators/LHCGenerator:NumberOfEvents 5', 
+        'set /Herwig/Generators/LHCGenerator:DebugLevel 2', 
+        'set /Herwig/Generators/LHCGenerator:PrintEvent 1', 
+        'set /Herwig/Generators/LHCGenerator:MaxErrors 10000'),
+    hwpp_pdf_CTEQ6LL = cms.vstring('+hwpp_pdf_CTEQ6L1')
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RECOSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
